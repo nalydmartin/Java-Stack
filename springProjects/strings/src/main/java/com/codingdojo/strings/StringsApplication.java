c179b9d83ed59d861e421b7fe4aebf56c49238ad
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2020-12-06 21:13:28 -0800

    Added springProjects folder

---
+++ springProjects/strings/src/main/java/com/codingdojo/strings/StringsApplication.java
@@ -0,0 +1,28 @@
+package com.codingdojo.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+
+@RestController
+public class StringsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+
+	
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Worlds!";
+	}
+	
+	
+	@RequestMapping("/random")
+	public String random() {
+		return "Help! I'm trapped inside the computer and I can't get out!!!";
+	}
+}
