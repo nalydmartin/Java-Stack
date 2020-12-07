4b1289eaaff74b31b6b19c2da57ecd675eaa8b71
Dylan Martin <69947551+nalydmartin@users.noreply.github.com>
2020-12-06 18:20:37 -0800

    Added Calculator 1 assignment

---
+++ JavaOOPADV/Calculator/src/com/codingdojo/calculator/Calculator.java
@@ -0,0 +1,66 @@
+package com.codingdojo.calculator;
+
+public class Calculator implements java.io.Serializable{
+
+	private double operandOne;
+	private double operandTwo;
+	private String operator;
+	private double sum;
+	
+	// Constructors 
+	
+	public Calculator() {
+		
+	}
+	
+	public Calculator(double num1, double num2) {
+		operandOne = num1;
+		operandTwo = num2;
+	}
+	
+	
+	// Setters
+	
+	public void setOperandOne(double number) {
+		operandOne = number;
+	}
+	
+	
+	public void setOperandTwo(double number) {
+		operandTwo = number;
+	}
+	
+	
+	public void setOperation(String math) {
+		operator = math;
+	}
+	
+	
+	// Getters 
+	
+	public double getOperandOne() {
+		return operandOne;
+	}
+	
+	
+	public double getOperandTwo() {
+		return operandTwo;
+	}
+	
+	public double getResults() {
+		return sum;
+	}
+	
+	// Perform operations
+	
+	public void performOperation() {
+		if (operator == "+") {
+			sum = operandOne + operandTwo;
+		} else if (operator == "-") {
+			sum = operandOne - operandTwo;
+		}
+	}
+	
+	
+	
+}
---
+++ JavaOOPADV/Calculator/src/com/codingdojo/calculator/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.codingdojo.calculator;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calculator calc1 = new Calculator();
+		
+		Calculator calc2 = new Calculator(9, 10);
+		
+		
+		// Test for calc1 
+		
+		calc1.setOperandOne(50);
+		calc1.setOperandTwo(20);
+		calc1.setOperation("-");
+		calc1.performOperation();
+		System.out.println(calc1.getResults());
+		
+		
+		// Test for calc2
+		
+		calc2.setOperation("+");
+		calc2.performOperation();
+		System.out.println(calc2.getResults());
+		
+		
+		
+	}
+
+}
